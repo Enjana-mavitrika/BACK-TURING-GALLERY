eb1ecc2d5a73ec82a0314496960fddc4d88f64f7
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2020-01-13 00:45:40 +0100

    fist commit

--- src/main/java/fr/ujm/turgal/DemoApplication.java
+++ src/main/java/fr/ujm/turgal/DemoApplication.java
@@ -2,22 +2,12 @@ package fr.ujm.turgal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@RestController
-	class helloController {
-		@GetMapping("/")
-		String hello() {
-			return "hello world";
-		}
 	}
 
 }
