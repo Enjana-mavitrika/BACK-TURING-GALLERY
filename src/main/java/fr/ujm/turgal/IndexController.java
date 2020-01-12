098a3a9ec966a44a4ce56a864467d9850c4756d9
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2020-01-13 00:16:11 +0100

    fist commit

--- src/main/java/fr/ujm/turgal/IndexController.java
+++ src/main/java/fr/ujm/turgal/IndexController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String indexAction() {
-        return "welcome";
-    }
+    // @GetMapping("/")
+    // public String indexAction() {
+    // return "welcome";
+    // }
 }
